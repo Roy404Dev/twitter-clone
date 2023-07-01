171c71f8341f64dddb3ed821c6077ddeb93b0722
Roy404Dev <rojusbalciunas@gmail.com>
2023-07-01 16:40:37 +0300

    modified

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/twitter-clone/",
   plugins: [react()],
-  server: {
-    port: 3000
-  },
-  base: "./"
-})
+});
